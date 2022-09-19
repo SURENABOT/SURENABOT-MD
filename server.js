0e48a4ce678370959b847701e361665fefd2f558
SURENA <113533619+SURENABOT@users.noreply.github.com>
2022-09-19 13:23:26 +0430

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+/** 
+ * Dont't edit or remove this file
+ * -------------------------------
+ * 
+ * Special thanks to adiwajshing/Baileys for Baileys (library)
+ *
+ * GITHUB 😺
+ * 
+ * 
+ * Surena: https://github.com/SURENABOT
+ * Creator: https://github.com/nextamir
+ * Baileys: https://github.com/adiwajshing/Baileys
+ * -----------------------------------------------
+ * 
+ * Surena is the first and best Iranian WhatsApp bot with full customization
+ * You can use and customize Surena for your personal or business needs
+ * Don't forget to give a star on our github page for this reposity
+ * ---------------------------------------------------------------
+ * 
+ * Creator WaLink: https://wa.me/989301860610
+ * Surena Instagram: https://instagram.com/surenabot
+ * -------------------------------------------------
+ * 
+ * © SURENABOT (GNU General Public License v3.0)
+ * 
+ * ┌─────────────────────────────────────────────────────────────────────────────────────┐
+ * │┌───────────────────────────────────────────────────────────────────────────────────┐│
+ * ││                                                                                   ││
+ * ││  ┌───────────┐   ┌─┐     ┌─┐   ┌─────────┐  ┌───────┐ ┌───     ┌─┐        /\      ││
+ * ││  │    ────┌─┐│   │ │     │ │   │ ┌─────┐ │  │ ┌─────┘ │   \    │ │       /  \     ││
+ * ││  \     ───┘ └┘   │ │     │ │   │ └─────┘ │  │ │       │ │\ \   │ │      / /\ \    ││
+ * ││   \     \        │ │     │ │   │ ┌──- -──┘  │ └─────┐ │ │ \ \  │ │     / /──\ \   ││
+ * ││    \     \       │ │     │ │   │ │   \ \    │ ┌─────┘ │ │  \ \ │ │    / ────── \  ││
+ * ││ ┌────     \      │ └─────┘ │   │ │    \ \   │ └─────┐ │ │   \ \│ │   / /      \ \ ││
+ * ││ └──────────┘     └─────────┘   └─┘     \_\  └───────┘ └─┘    ────┘   ──        ── ││
+ * ││                                                                                   ││
+ * │└─────────────────────────────────── WHATSAPP BOT ──────────────────────────────────┘│
+ * └─────────────────────────────────────────────────────────────────────────────────────┘
+ * */
+
+const _0x215bcf=_0x55f5;(function(_0x33b39f,_0x111cf1){const _0x1e71f1=_0x55f5,_0x5917cd=_0x33b39f();while(!![]){try{const _0x5dc8af=parseInt(_0x1e71f1(0xc5))/0x1*(parseInt(_0x1e71f1(0xb5))/0x2)+parseInt(_0x1e71f1(0xa9))/0x3+-parseInt(_0x1e71f1(0xa4))/0x4*(parseInt(_0x1e71f1(0xb2))/0x5)+parseInt(_0x1e71f1(0xbd))/0x6*(parseInt(_0x1e71f1(0xa7))/0x7)+parseInt(_0x1e71f1(0xb4))/0x8*(-parseInt(_0x1e71f1(0xa5))/0x9)+parseInt(_0x1e71f1(0xb6))/0xa*(-parseInt(_0x1e71f1(0xa8))/0xb)+parseInt(_0x1e71f1(0xb1))/0xc;if(_0x5dc8af===_0x111cf1)break;else _0x5917cd['push'](_0x5917cd['shift']());}catch(_0x38f186){_0x5917cd['push'](_0x5917cd['shift']());}}}(_0x1839,0xe72e4));let express=require('express'),path=require(_0x215bcf(0xa6)),SocketIO=require(_0x215bcf(0xc2)),qrcode=require(_0x215bcf(0xa2)),fetch=require(_0x215bcf(0xba));function connect(_0x2e8b9d,_0x314d75){const _0x15c7c9=_0x215bcf;let _0x29ae89=global[_0x15c7c9(0xc4)]=express(),_0x447697=_0x15c7c9(0xa3);_0x2e8b9d['ev']['on'](_0x15c7c9(0xaa),function _0x466d99({qr:_0x441bf6}){if(_0x441bf6)_0x447697=_0x441bf6;}),_0x29ae89[_0x15c7c9(0xaf)](async(_0x56e73f,_0x43c18f)=>{const _0x32bdef=_0x15c7c9;_0x43c18f[_0x32bdef(0xae)](_0x32bdef(0xab),_0x32bdef(0xad)),_0x43c18f[_0x32bdef(0xbc)](await qrcode['toBuffer'](_0x447697));});let _0x4b7a3e=_0x29ae89[_0x15c7c9(0xac)](_0x314d75,()=>{const _0x5cd407=_0x15c7c9;console['log'](_0x5cd407(0xb9),_0x314d75);if(opts[_0x5cd407(0xb0)])keepAlive();}),_0x43ae8c=SocketIO(_0x4b7a3e);_0x43ae8c['on'](_0x15c7c9(0xc3),_0x1527e0=>{const _0x304a95=_0x15c7c9;let {unpipeEmit:_0x13f90f}=pipeEmit(_0x2e8b9d,_0x1527e0,_0x304a95(0xb3));_0x1527e0['on'](_0x304a95(0xbe),_0x13f90f);});}function _0x55f5(_0x872a77,_0x246c8c){const _0x183939=_0x1839();return _0x55f5=function(_0x55f5cf,_0x4f0671){_0x55f5cf=_0x55f5cf-0xa2;let _0x29e563=_0x183939[_0x55f5cf];return _0x29e563;},_0x55f5(_0x872a77,_0x246c8c);}function pipeEmit(_0x66cc4e,_0x126653,_0x2fdad7=''){const _0x50b111=_0x215bcf;let _0x16c25f=_0x66cc4e[_0x50b111(0xb7)];return _0x66cc4e[_0x50b111(0xb7)]=function(_0xba0aba,..._0x19224b){const _0x145797=_0x50b111;_0x16c25f[_0x145797(0xb7)](_0xba0aba,..._0x19224b),_0x126653[_0x145797(0xb7)](_0x2fdad7+_0xba0aba,..._0x19224b);},{'unpipeEmit'(){const _0x54bd62=_0x50b111;_0x66cc4e[_0x54bd62(0xb7)]=_0x16c25f;}};}function _0x1839(){const _0x3d5678=['qrcode','invalid','319408xMzUQN','12966579khqugN','path','5814690xuOeLl','2934448ouiBSi','1630239ZkgbCS','connection.update','content-type','listen','image/png','setHeader','use','keepalive','32555892NoTLJB','95bXpoxz','conn-','8JvrBre','2pyWQkb','10GNArwN','emit','catch','Application\x20running\x20on\x20port\x20','node-fetch','https://','end','6AuzRYU','disconnect','env','.repl.co','error','socket.io','connection','app','84529HdINMs','test'];_0x1839=function(){return _0x3d5678;};return _0x1839();}function keepAlive(){const _0x546174=_0x215bcf,_0x34aed1=_0x546174(0xbb)+process[_0x546174(0xbf)]['REPL_SLUG']+'.'+process[_0x546174(0xbf)]['REPL_OWNER']+_0x546174(0xc0);if(/(\/\/|\.)undefined\./[_0x546174(0xc6)](_0x34aed1))return;setInterval(()=>{const _0x3dc819=_0x546174;fetch(_0x34aed1)[_0x3dc819(0xb8)](console[_0x3dc819(0xc1)]);},0x5*0x3e8*0x3c);}module['exports']=connect;
